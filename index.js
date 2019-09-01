e672421cc9e6688674d9a46e7b6b32abc62984d3
Chris <chris.greindl@gmail.com>
2019-09-01 09:27:42 -0300

    Use index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const visit = require('unist-util-visit');
+
+const applyClassesToNode = require('./src/applyClassesToNode');
+
+module.exports = ({ markdownAST }, { classMap = {} }) => {
+  // @see: https://github.com/syntax-tree/mdast#nodes
+  visit(markdownAST, `heading`, (node) => {
+    const selector = `h${node.depth}`;
+
+    if (selector in classMap) {
+      node = applyClassesToNode(node, classMap[selector]);
+    }
+  });
+
+  visit(markdownAST, `table`, (node) => {
+    const selector = `table`;
+
+    if (selector in classMap) {
+      node = applyClassesToNode(node, classMap[selector]);
+    }
+  });
+
+  visit(markdownAST, `paragraph`, (node) => {
+    const selector = `paragraph`;
+
+    if (selector in classMap) {
+      node = applyClassesToNode(node, classMap[selector]);
+    }
+  });
+};
